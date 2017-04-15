794324c8c4f09c4670b36faf08d5ce58754bc80f
Qiang Lu <buaaluqiang@hotmail.com>
2017-04-15 08:22:02 -0600

    feat: add compute RMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,26 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd RMSE(4);
+  RMSE << 0,0,0,0;
+  
+  // check validity of the inputs;
+  if(estimations.size() != ground_truth.size() || estimations.size() < 1){
+    std::cout << "invalid ground truth, please check" << std::endl;
+    return RMSE;
+  }
+  
+  // accumulating squared residuals
+  for(int i=0; i < estimations.size(); ++i){
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array() * residual.array();
+    RMSE += residual;
+  }
+  
+  // mean and squared root
+  RMSE = RMSE / estimations.size();
+  RMSE = RMSE.array().sqrt();
+  
+  return RMSE;
+
 }
